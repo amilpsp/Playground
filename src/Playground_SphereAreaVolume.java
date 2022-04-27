826651be8bb46e866af8357ef43a263eab7a36c7
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-04-27 16:53:10 +0200

    Reworked the sphere excercise done in class.

--- src/Playground_SphereAreaVolume.java
+++ src/Playground_SphereAreaVolume.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class Playground_SphereAreaVolume {
 
-    public static void main(String[]args){
+    static double requestRadiusValue(){
+        Scanner rVarde = new Scanner(System.in);
+        System.out.println("Bestäm värden av klotets radius: ");
+        double radius = rVarde.nextDouble();
+        return radius;
+    }
 
+    public static void main(String[]args){
         //KlotArea = 4 · pi · r²
         //KlotVolym = (4 · pi · r³)/3
 
-        Scanner rVarde = new Scanner(System.in);
-        System.out.println("Bestäm värden av klotets radius: ");
-        double r = rVarde.nextDouble();
+        double radius = requestRadiusValue();
 
-        CustomClass_Sphere globe = new CustomClass_Sphere(r);
+        CustomClass_Sphere globe = new CustomClass_Sphere(radius);
+
         int areaRounded = (int)globe.computeArea();
         int volumeRounded = (int)globe.computeVolume();
 
