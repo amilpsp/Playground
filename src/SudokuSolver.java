1e473aba61d4ce3542164a812b5674af9d43ff5f
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-05-11 01:35:08 +0200

    @Sanders. Try, catch, errors, java.io, writing files... new stuff to learn

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -14,6 +14,8 @@ public class SudokuSolver {
                 { 0, 0, 7, 0, 4, 0, 2, 0, 3 },
         };//board
 
+        printBoard(board);
+
         if (solveBoard(board))
             System.out.println("Solved successfully!");
         else
